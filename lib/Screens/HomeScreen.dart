555117a823cc680740b44febdf8acabd1d0d239c
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2017-11-19 11:20:32 +0000

    Update HomeScreen.dart

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -13,7 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orangeAccent,
-        title: Text('Football App 02'),
+        title: Text('Football App 12'),
         automaticallyImplyLeading: false,
         leading: Navigator.canPop(context)
             ? IconButton(
